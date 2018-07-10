392dc83e174334751f96ae7bcc57891baac6e178
Yomar Gutierrez Mejia <yomarguti@Yomars-MacBook-Pro.local>
2018-07-10 15:15:08 -0500

    Fix error on report date

--- playground.js
+++ playground.js
@@ -10,4 +10,7 @@ console.log(moment("2018-07-10T00:55:00.000Z").tz('America/Bogota').format('LLLL
 console.log(moment("2018-07-09T00:55:00.000Z").unix());
 console.log(moment.utc("2018-07-09T19:55:00.000-05").format('LLLL'))
 
-
+var nameUser = undefined;
+const hora = `${moment().tz('America/Bogota').format('hA')}`;
+const strStart = nameUser? `Hola @${nameUser}, aquí tienes los datos del clima ahora:\n` : `Reporte ${hora} del cilma:\n`;
+console.log(strStart);
